27dfda9c3c55986436c5f02473fbfd4f9ec3f311
Matteo Semeraro <sgarrotxt@gmail.com>
2016-04-05 20:57:56 +0200

    added user to database

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://Sgarro:sgarro24pwd@ds019468.mlab.com:19468/personaltrainer');
+mongoose.connect('mongodb://Daniele:danielepwd@ds019468.mlab.com:19468/personaltrainer');
